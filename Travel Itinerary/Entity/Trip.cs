d70aaf37917a447b1c99d9ad6c4ac82d9ccec304
nigissh <125720887+nigissh@users.noreply.github.com>
2023-03-18 16:34:44 +0600

    Update Trip.cs

--- Travel Itinerary/Entity/Trip.cs
+++ Travel Itinerary/Entity/Trip.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
+        public Destination Destination { get; set; }
+        public int DestinationId { get; set; }
     }
 }
